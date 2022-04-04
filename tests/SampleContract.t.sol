40ab146073c025f702836e3372d327282c5cc849
Lucas Manuel <lucas@maple.finance>
2022-04-04 16:48:10 -0400

    feat: initial treasury setup

--- tests/SampleContract.t.sol
+++ tests/SampleContract.t.sol
@@ -2,21 +2,47 @@
 pragma solidity ^0.8.7;
 
 import { TestUtils } from "../modules/contract-test-utils/contracts/test.sol";
+import { IERC20 }    from "../modules/erc20/contracts/interfaces/IERC20.sol";
+import { Staker }    from "../modules/revenue-distribution-token/contracts/test/accounts/Staker.sol";
+import { xMPL }      from "../modules/xmpl/contracts/xMPL.sol";
+import { xMPLOwner } from "../modules/xmpl/contracts/test/accounts/Owner.sol";
 
-contract ERC20BaseTest is TestUtils { 
+import { AddressRegistry } from "../contracts/AddressRegistry.sol";
 
-    function setUp() public virtual { }
+import { IBPoolLike, IMapleTreasuryLike } from "../contracts/interfaces/Interfaces.sol";
 
-    function test_success() external {
-        assertTrue(true);
+contract xMPLSimulation is AddressRegistry, TestUtils {
+
+    IBPoolLike         bPool    = IBPoolLike(BALANCER_POOL);
+    IERC20             mpl      = IERC20(MPL);
+    IERC20             usdc     = IERC20(USDC);
+    IMapleTreasuryLike treasury = IMapleTreasuryLike(MAPLE_TREASURY);
+
+    Staker    staker;
+    xMPL      xmpl;
+    xMPLOwner owner;
+
+    function setUp() public virtual {
+        staker = new Staker();
+        owner  = new xMPLOwner();
+        xmpl   = new xMPL("xMPL", "xMPL", address(owner), address(mpl), 1e30);
     }
 
-    function test_failure() external {
-        assertTrue(false);
-    }
+    function test_setUpVesting() public {
+        erc20_mint(USDC, 9, MAPLE_TREASURY, 1_000_000e6);
 
-    function test_fuzz(uint256 x) external {
-        assertEq(x, x);
+        vm.startPrank(GOVERNOR);
+        treasury.reclaimERC20(USDC, 1_000_000e6);
+
+        usdc.approve(address(bPool), 1_000_000e6);
+
+        bPool.swapExactAmountIn({
+            tokenIn:       USDC,
+            tokenAmountIn: 1000e6,
+            tokenOut:      MPL,
+            minAmountOut:  0,
+            maxPrice:      type(uint256).max
+        });
     }
 
 }
