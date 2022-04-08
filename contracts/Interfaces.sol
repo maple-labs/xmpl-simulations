665d0e208bd1624ace06a556c0ae1d5dcb1fe8cb
Vedran Bidin <vedran@bidin.me>
2022-04-08 12:54:53 -0500

    fix: update interfaces

---
+++ contracts/Interfaces.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.7;
+
+interface IBPoolLike {
+
+    function swapExactAmountIn(
+        address tokenIn,
+        uint256 tokenAmountIn,
+        address tokenOut,
+        uint256 minAmountOut,
+        uint256 maxPrice
+    )
+        external
+        returns (uint tokenAmountOut, uint spotPriceAfter);
+}
+
+interface IMapleTreasuryLike {
+
+    function reclaimERC20(address asset_, uint256 amount_) external;
+
+}
